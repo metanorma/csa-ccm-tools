0b49c41ddb2c280226946a7bb0b980e3dec8cadf
Alexande B <abobrikovich@gmail.com>
2019-05-01 22:25:16 +0300

    Add xlsx2yaml command

--- lib/csa/ccm/cli/command.rb
+++ lib/csa/ccm/cli/command.rb
@@ -22,7 +22,7 @@ module Csa
           input_file = input_files.first
 
           unless File.exist? input_file
-            UI.say('No file found for version')
+            UI.say("#{input_file} doesn't exists for #{version} version")
             return
           end
 
@@ -35,8 +35,17 @@ module Csa
         desc "xlsx2yaml XSLT_PATH", "Converting CCM XSLX to YAML"
         option :output_file, aliases: :o, type: :string, desc: "Optional output YAML file. If missed, the input file’s name will be used"
 
-        def xlsx2yaml(xslt_path)
-          raise 'Not implemented yet'
+        def xlsx2yaml(input_xlsx_file)
+          unless input_xlsx_file
+            UI.say("#{input_xlsx_file} file doesn't exists")
+            return
+          end
+
+          version = input_xlsx_file[/(?<=v)[0-9\.]*(?=-)/] || 'unknown'
+          matrix = Matrix.from_xlsx(version, input_xlsx_file)
+
+          output_file = options[:output_file] || input_xlsx_file.gsub('.xlsx', '.yaml')
+          matrix.to_file(output_file)
         end
 
         desc "caiq2yaml XSLT_PATH", "Converting a filled CAIQ to YAML"
@@ -44,62 +53,7 @@ module Csa
         option :output_path, aliases: :p, type: :string, desc: "Optional output directory for result file. If missed pwd will be used"
 
         def caiq2yaml(xslt_path)
-          # if xslt_path.nil?
-          #   puts 'Error: no filepath given as first argument.'
-          #   exit 1
-          # end
 
-          # if Pathname.new(xslt_path).extname != ".xlsx"
-          #   puts 'Error: filepath given must have extension .xlsx.'
-          #   exit 1
-          # end
-
-          # workbook = Csa::Ccm::MatrixWorkbook.new(xslt_path)
-          # workbook.glossary_info.metadata_section.structure
-          # workbook.glossary_info.metadata_section.attributes
-
-          # languages = {}
-
-          # workbook.languages_supported.map do |lang|
-          #   puts "************** WORKING ON LANGUAGE (#{lang})"
-          #   sheet = workbook.language_sheet(lang)
-          #   termsec = sheet.terms_section
-          #   languages[sheet.language_code] = termsec.terms
-          # end
-
-          # collection = Csa::Ccm::ControlDomain.new
-
-          # languages.each_pair do |lang, terms|
-          #   terms.each do |term|
-          #     collection.add_term(term)
-          #   end
-          # end
-
-          # # collection[1206].inspect
-
-          # # FIXME use  instea output dir
-          # output_dir = options[:output_path] || Dir.pwd
-          # output_file = options[:output_name] || File.join(output_dir, Pathname.new(filepath).basename.sub_ext(".yaml"))
-
-          # collection.to_file(output_file)
-
-          # collection_output_dir = File.join(output_dir, "concepts")
-
-          # FileUtils.mkdir_p(collection_output_dir)
-
-          # collection.keys.each do |id|
-          #   collection[id].to_file(File.join(collection_output_dir, "concept-#{id}.yaml"))
-          # end
-
-          # french = workbook.language_sheet("French")
-          # french.sections[3].structure
-          # french.sections[3].terms
-
-          # english = workbook.language_sheet("English")
-          # english.terms_section
-          # english.terms_section.terms
-
-          #pry.binding
         end
 
         desc "generate-with-answers ANSWERS_YAML", "Writing to the CAIQ XSLX template using YAML"
--- spec/csa/ccm/cli/command_spec.rb
+++ spec/csa/ccm/cli/command_spec.rb
@@ -1,18 +1,49 @@
 require 'spec_helper'
 
 RSpec.describe Csa::Ccm::Cli do
+	before(:all) do
+    FileUtils.mkdir_p './tmp'
+  end
+
+  after(:all) do
+    FileUtils.rm_rf './tmp'
+    FileUtils.rm './caiq-3.0.1.yaml'
+    FileUtils.rm './resources/csa-caiq-v3.0.1-12-05-2016.yaml'
+  end
+
   it 'has a version number' do
     expect(Csa::Ccm::Cli::VERSION).not_to be nil
   end
 
-  it 'ccm-yaml 3.0.1 -o ccm-301.yaml' do
+  it 'command properly implemented' do
     allow(Csa::Ccm::Cli::Command).to receive(:run)
+  end
 
-    FileUtils.mkdir_p './tmp'
+  it 'ccm-yaml 3.0.1' do
+    command = %w[ccm-yaml 3.0.1]
+    capture_stdout { Csa::Ccm::Cli::Command.start(command) }
 
+    expect(File.exist?('./caiq-3.0.1.yaml')).to be_truthy
+  end
+
+  it 'ccm-yaml 3.0.1 -o ccm-301.yaml' do
     command = %w[ccm-yaml 3.0.1 -o ./tmp/ccm-301.yaml]
     capture_stdout { Csa::Ccm::Cli::Command.start(command) }
 
     expect(File.exist?("./tmp/ccm-301.yaml")).to be_truthy
+  end
+
+  it 'xlsx2yaml ./resources/csa-caiq-v3.0.1-12-05-2016.xlsx' do
+    command = %w[xlsx2yaml ./resources/csa-caiq-v3.0.1-12-05-2016.xlsx]
+    capture_stdout { Csa::Ccm::Cli::Command.start(command) }
+
+    expect(File.exist?('./resources/csa-caiq-v3.0.1-12-05-2016.yaml')).to be_truthy
+  end
+
+  it 'xlsx2yaml ./resources/csa-caiq-v3.0.1-12-05-2016.xlsx -o ./tmp/ccm-301-2.yaml' do
+    command = %w[xlsx2yaml ./resources/csa-caiq-v3.0.1-12-05-2016.xlsx -o ./tmp/ccm-301-2.yaml]
+    capture_stdout { Csa::Ccm::Cli::Command.start(command) }
+
+    expect(File.exist?('./tmp/ccm-301-2.yaml')).to be_truthy
   end
 end
